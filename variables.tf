70fefc241ea7e6b85a6c81314d261cfc25486547
pasunuriswathi2-byte <pasunuriswathi2@gmail.com>
2025-11-07 15:04:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,25 +1,21 @@
 variable "project_id" {
   description = "terraform-vm-project-477405"
   type        = string
-  default     = "terraform-vm-project-477405"
 }
 
 variable "region" {
   description = "us-east1"
   type        = string
-  default     = "us-east1"
 }
 
 variable "zone" {
   description = "us-east1-a"
   type        = string
-  default     = "us-east1-a"
 }
 
 variable "vm_name" {
   description = "VM0"
   type        = string
-  default     = "VM0"
 }
 
 variable "machine_type" {

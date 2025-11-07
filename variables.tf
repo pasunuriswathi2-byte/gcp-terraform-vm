16a961d53df4adda2918ad8708548664fb2d6f2c
pasunuriswathi2-byte <pasunuriswathi2@gmail.com>
2025-11-07 14:57:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,28 +1,51 @@
 variable "project_id" {
-  description = "GCP project id"
+  description = "terraform-vm-project-477405"
   type        = string
+  default     = "terraform-vm-project-477405"
 }
 
 variable "region" {
-  description = "GCP region"
+  description = "us-east1"
   type        = string
-  default     = "us-central1"
+  default     = "us-east1"
 }
 
 variable "zone" {
-  description = "GCP zone"
+  description = "us-east1-a"
   type        = string
-  default     = "us-central1-a"
+  default     = "us-east1-a"
+}
+
+variable "vm_name" {
+  description = "VM0"
+  type        = string
+  default     = "VM0"
 }
 
 variable "machine_type" {
-  description = "Machine type"
+  description = "Machine type for the VM"
   type        = string
   default     = "e2-medium"
 }
 
-variable "gcp_credentials_file" {
-  description = "Path to GCP service account JSON key (used for local runs). Jenkins will use credentials differently."
+variable "ssh_user" {
+  description = "Username for SSH access"
   type        = string
-  default     = "terraform-jenkins-sa.json"
+}
+
+variable "ssh_public_key_path" {
+  description = "Path to your public SSH key"
+  type        = string
+}
+
+variable "image_family" {
+  description = "Image family for the VM"
+  type        = string
+  default     = "debian-12"
+}
+
+variable "image_project" {
+  description = "Project where the image resides"
+  type        = string
+  default     = "debian-cloud"
 }
